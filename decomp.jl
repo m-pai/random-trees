1d85fd5ae1547dacb809fc51e521bf59a047c0f6
thanksf0rallthefish <99610260+m-pai@users.noreply.github.com>
2023-11-06 23:25:38 +0530

    updated decomp.jl

--- decomp.jl
+++ decomp.jl
@@ -8,10 +8,10 @@ function decompositions(tree_size, bag_size)
   ngs = reflexive_conv(bag_size)
 
   for gs in Combinatorics.combinations(ngs, tree_size)
-    τ = Catlab.CategoricalAlgebra.FinCats.graph(∫(T))
-    ob_dict = Dict(zip(ob_generators(∫(T)), gs))
-    for f in hom_generators(elements(T))
-      all_epis = filter(is_epic, homomorphisms(ob_dict[dom(f)], ob_dict[codom(f)]))
+    el = elements(T)
+    ob_dict = Dict(zip(ob_generators(∫(el)), gs))
+    for f in hom_generators(∫(el))
+      all_epis = filter(is_epic, homomorphisms(ob_dict[el[f, :src]], ob_dict[el[f, :tgt]]))
     end
   end
 
