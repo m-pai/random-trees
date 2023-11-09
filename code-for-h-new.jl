d7e7a6569fac640a6524cd0226464bcdda4c10b8
thanksf0rallthefish <99610260+m-pai@users.noreply.github.com>
2023-11-09 22:04:14 +0530

    decomp.jl edit; function for h

--- code-for-h-new.jl
+++ code-for-h-new.jl
@@ -9,3 +9,16 @@ end
 B = [1:2, 1:2, 1:4]
 
 A = collect(IterTools.Iterators.product(B...))
+
+function h(lengths)
+    k = []
+    for i in lengths
+        push!(k, (1:i)) 
+    end
+    A = collect(IterTools.Iterators.product(k...))
+    return A
+end
+
+n = [1,2,3]
+
+h(n)
--- decomp.jl
+++ decomp.jl
@@ -1,20 +1,31 @@
-function decompositions(tree_size, bag_size)
-  decomp = []
 
-  # generating random tree of size m
-  T = rand_tree(tree_size)
+tree_size = 5
+bag_size = 3
 
-  # array of reflexive graphs of size n
-  ngs = reflexive_conv(bag_size)
+decomp = []
 
-  for gs in Combinatorics.combinations(ngs, tree_size)
-    el = elements(T)
-    ob_dict = Dict(zip(ob_generators(∫(el)), gs))
-    for f in hom_generators(∫(el))
-      all_epis = filter(is_epic, homomorphisms(ob_dict[el[f, :src]], ob_dict[el[f, :tgt]]))
-    end
+# generating random tree of size m
+T = rand_tree(tree_size)
+
+# array of reflexive graphs of size n
+ngs = reflexive_conv(bag_size)
+
+for gs in Combinatorics.combinations(ngs, tree_size + tree_size - 1)
+  el = elements(T)
+  ob_dict = Dict(zip(ob_generators(∫(el)), gs))
+  for f in hom_generators(∫(el))
+    all_epis = filter(is_epic, homomorphisms(ob_dict[el[f, :src]], ob_dict[el[f, :tgt]]))
+    # Doubt 1: I think the is_epic filter is returning an empty all_epis vector which is why it is aborting every time.
+    if all_epis == 0 break
+    else 
+      lengths = map(length, all_epis)
+      for i in h(lengths) 
+        mor_dict = Dict(zip()) # Doubt 2: I need help coding this.
+        F = FinDomFunctor(ob_dict, mor_dict, el) 
+        d = StrDecomp(T,F)
+        append(d, decomp)
+      end
+    end 
   end
-
-  
 end
 
