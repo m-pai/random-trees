ab5e0e659a17d7cc5d8ea2c7c856c852e0ee207c
thanksf0rallthefish <99610260+m-pai@users.noreply.github.com>
2023-11-18 00:24:44 +0530

    decomp.jl edited file

--- decomp.jl
+++ decomp.jl
@@ -1,3 +1,4 @@
+
 function decompositions(tree_size, bag_size)
   decomp = []
 
@@ -7,17 +8,21 @@ function decompositions(tree_size, bag_size)
   # array of reflexive graphs of size n
   ngs = reflexive_conv(bag_size)
 
+  push!(ngs, ngs[1])
+  push!(ngs, ngs[2])
+
+  # Error: size of ngs smaller than size of v+e and so not entering for loop
   for gs in Combinatorics.combinations(ngs, tree_size + tree_size - 1)
     el = elements(T)
     ob_dict = Dict(zip(ob_generators(∫(el)), gs))
     for f in hom_generators(∫(el))
-      all_epis = filter(is_epic, list_of_homs(ob_dict[el[f, :src]], ob_dict[el[f, :tgt]]))
-      # Doubt 1: I think the is_epic filter is returning an empty all_epis vector which is why it is aborting every time.
+      all_epis = filter(t -> is_epic(t), homomorphisms(ob_dict[el[f, :src]], ob_dict[el[f, :tgt]]))
+      print(all_epis)
       if all_epis == 0 break
       else 
         lengths = map(length, all_epis)
         for i in h(lengths) 
-          mor_dict = Dict(zip(morphisms(∫(el)), map((l,i) -> l[i], zip(all_epis, indices)))) #check code here 
+          mor_dict = Dict(zip(hom(el), map((l,i) -> l[i], zip(all_epis, i)))) #check code here 
           F = FinDomFunctor(ob_dict, mor_dict, el) 
           d = StrDecomp(T,F)
           append(d, decomp)
