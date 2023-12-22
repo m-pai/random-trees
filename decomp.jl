14920736e9353b78fa16fa961f30805d7be85486
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2023-12-22 14:10:18 -0500

    Fixed decomp code
    Code now works, however this a problem with the graph generation in that it is rarely finding epimorphisms between graphs.

--- decomp.jl
+++ decomp.jl
@@ -1,3 +1,11 @@
+include("Decompositions.jl")
+using .Decompositions
+using Catlab.CategoricalAlgebra
+
+include("random-trees-new.jl")
+include("graph-gen-new.jl")
+include("code-for-h-new.jl")
+
 function decompositions(tree_size, bag_size)
   decomp = []
 
@@ -7,30 +15,32 @@ function decompositions(tree_size, bag_size)
   # array of reflexive graphs of size n
   ngs = reflexive_conv(bag_size)
 
-  all_epis = Any[]
 
-  graph_assign = []
-
-  for i in range(1,2*tree_size-1)
-    k = rand(ngs)
-    push!(graph_assign, k)
-  end
+  num_tree_edges_vertices = 2 * tree_size - 1
+  graph_assign = rand(ngs, num_tree_edges_vertices)
   print(graph_assign)
 
-  for gs in Combinatorics.multiset_permutations(graph_assign, tree_size + tree_size-1)
-    el = elements(T)
-    ob_dict = Dict(zip(ob_generators(∫(el)), gs))
-    for f in hom_generators(∫(el))
+  el = elements(T)
+  ∫el = ∫(el)
+  ob_gen_el = ob_generators(∫el)
+  hom_gen_el = hom_generators(∫el)
+
+  for gs in Combinatorics.permutations(graph_assign, num_tree_edges_vertices)
+  # for gs in [[ngs[1], ngs[1], ngs[1], ngs[1], ngs[1]]]
+    ob_dict = Dict(zip(ob_gen_el, gs))
+    all_epis = Any[]
+    for f in hom_gen_el
       push!(all_epis, filter(t -> is_epic(t), homomorphisms(ob_dict[el[f, :src]], ob_dict[el[f, :tgt]])))
     end
-    if all_epis != Any[] 
+    if !isempty(all_epis)
       lengths = map(length, all_epis)
-      print(lengths)
+      println(lengths)
       for i in h(lengths) 
-        mor_dict = Dict(zip(hom_generators(∫(T)), map((l,i) -> l[i], zip(all_epis, i))))
-        F = FinDomFunctor(ob_dict, mor_dict, ∫(T)) 
+        chosen_epis = map(l -> l[1][l[2]], zip(all_epis, i))
+        mor_dict = Dict(zip(hom_gen_el, chosen_epis))
+        F = FinDomFunctor(ob_dict, mor_dict, ∫el) 
         d = StrDecomp(T,F)
-        append(d, decomp)
+        push!(decomp, d)
       end
     end
   end
