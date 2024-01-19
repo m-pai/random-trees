ea355203e376c61d4cefb07a9141e079819f0956
thanksf0rallthefish <99610260+m-pai@users.noreply.github.com>
2024-01-20 00:37:10 +0530

    limit

---
+++ limits.jl
@@ -0,0 +1,16 @@
+include("Decompositions.jl")
+using .Decompositions
+using Catlab.CategoricalAlgebra
+
+include("random-trees-new.jl")
+include("graph-gen-new.jl")
+include("code-for-h-new.jl")
+include("decomp.jl")
+
+function apex(sol)
+    arr = []
+    for i in sol 
+        push!(arr, Catlab.CategoricalAlgebra.FreeDiagrams.apex(Catlab.CategoricalAlgebra.Limits.limit(i)))
+    end
+    return arr
+end
